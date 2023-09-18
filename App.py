76ec331754933fc0068786b40c178f8b5e2b17f8
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-09-18 18:44:24 +0300

    Update and rename main.py to App.py

---
+++ App.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="GSB", page_icon="16.png")
+
+def main():
+    st.image("GSB Medium.png", width=120,)
+    st.title("Graduate School of Business 🏫")
+    components.iframe("https://app.powerbi.com/view?r=eyJrIjoiNzhmYzA2YzktMWJmOC00MGM2LWJmMWEtM2I3ZjhjZDk1MDgyIiwidCI6ImE0NzRkYzY0LTQ1ZDEtNDNkOS05N2FjLWM4NWY0ZDMyZmUwMCIsImMiOjl9", width=600, height=400)
+
+
+if __name__ == "__main__":
+    main()
