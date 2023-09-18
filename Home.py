0328b65156261a07d9106de0139ac95211875697
Hossam Daoud <101238592+HossamDaoud83@users.noreply.github.com>
2023-09-18 19:40:56 +0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="GSB", page_icon="16.png")
+
+st.image("GSB Medium.png", width=120,)
+st.title("Graduate School of Business 🏫")
+st.image("Power-Bi-Service.jpg", width=600,)
+
+
